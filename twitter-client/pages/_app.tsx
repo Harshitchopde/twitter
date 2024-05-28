eb403a3e98aa081ce6da5eb581bd4d10877c747f
Harshitchopde <chopdeharshit@gmail.com>
2024-05-28 23:43:37 +0530

    Success but hyration still fail

---
+++ twitter-client/pages/_app.tsx
@@ -0,0 +1,31 @@
+import type { AppProps } from 'next/app'
+import "./globals.css";
+import React from 'react';
+import { Provider } from '@/providers/provider';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Quicksand } from 'next/font/google';
+const quicksand = Quicksand({subsets:["latin"]});
+import { Toaster } from 'react-hot-toast';
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const queryClient = new QueryClient()
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <html lang="en">
+       <body className={quicksand.className}>
+      <Provider>
+        <GoogleOAuthProvider clientId="your-client-id-here">
+          <QueryClientProvider client={queryClient}>
+           
+              <Component {...pageProps} />
+              <Toaster />
+              <ReactQueryDevtools />
+           
+          </QueryClientProvider>
+        </GoogleOAuthProvider>
+      </Provider>
+      </body>
+      
+    </html>
+  );
+}
