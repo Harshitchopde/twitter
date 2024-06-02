30004e5b1dc5c2d4fa14ee2208497bafa428bb47
Harshitchopde <chopdeharshit@gmail.com>
2024-06-02 20:35:15 +0530

    latest updated tweet

--- twitter-client/pages/index.tsx
+++ twitter-client/pages/index.tsx
@@ -1,15 +1,20 @@
-"use client"
+// "use client"
 import FeedCard from "@/components/FeedCards";
 import CreatePost from "@/components/CreatePost";
 import { useGetAllTweets } from "@/hooks/tweer";
 import { Tweet } from "@/gql/graphql";
 import TweeterLayout from "@/components/Layouts/TweeterLayout";
+import { GetServerSideProps } from "next";
+import { graphqlClient } from "@/clients/api";
+import { getAllTweetsQuery } from "@/graphql/query/tweet";
 
+interface ServerPropsTweets{
+  tweets?: Tweet[]
+}
 
+export default function Home(props:ServerPropsTweets) {
 
-export default function Home() {
-  
-  const { tweets } = useGetAllTweets();
+    const { tweets } = props
 
   // console.log(user+"d")
 
@@ -26,4 +31,14 @@ export default function Home() {
        </TweeterLayout>
     </div>
   );
+}
+export const getServerSideProps:GetServerSideProps<ServerPropsTweets> =async(context)=>{
+  const tweets = await graphqlClient.request(getAllTweetsQuery);
+ 
+
+  return {
+    props:{
+        tweets:tweets.getAllTweets as Tweet[],
+    }
+  }
 }
